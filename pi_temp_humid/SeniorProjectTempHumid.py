315637026bbc956032bc2d57c510dc67bbd0fb81
Ben Howarth <benjamin.howarth@sjsu.edu>
2022-02-24 15:59:47 -0800

    Temp/Humid python (Pi) FWV

---
+++ pi_temp_humid/SeniorProjectTempHumid.py
@@ -0,0 +1,31 @@
+import time
+import board
+import adafruit_dht
+import psutil
+
+#kills previously running process.
+for proc in psutil.process_iter():
+    if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
+        proc.kill()
+
+#output pin on pi that sensor is connected to.        
+sensor = adafruit_dht.DHT11(board.D23)
+
+#Displays temperature/humiduty data or shows an error.
+while True:
+    try:
+        temp = sensor.temperature
+        humidity = sensor.humidity
+        print("Temperature: {}*C  Humidity: {}% ".format(temp, humidity))
+
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    
+    except Exception as error:
+        sensor.exit()
+        raise error
+    
+    time.sleep(2.0)
+        
