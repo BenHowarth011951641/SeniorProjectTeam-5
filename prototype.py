d6f5c530443f03cec1a54e2109c34a5b40b4fe2b
Ben Howarth <benjamin.howarth@sjsu.edu>
2022-04-04 21:08:25 -0700

    LoRa sending sensor data

--- prototype.py
+++ prototype.py
@@ -1,11 +1,14 @@
 import time
 from mq2 import init as initmq2, readCO2
 from TempHumid import measure, init as inittemp
+from sx126x import sx126x
 
 #main loop
 def main():
          initmq2()
          inittemp()
+         node = sx126x(serial_num = "/dev/ttyS0",freq=915,addr=2,power=22,rssi=True,air_speed=2400,relay=False)
+
          print("please wait for sensors to warm up...")
          time.sleep(20)
          while True:
@@ -13,14 +16,16 @@ def main():
                   print("Current Voltage = " +str("%.2f"%(COlevel))+" V")
                   
                   temp, humid  = measure()
-                  print("Temperature: {}*C  Humidity: {}% ".format(temp, humid))
+                  print("Temperature: {}C  Humidity: {}% ".format(temp, humid))
                   
                   if (COlevel > 1.8):
                       print("Fire detected!!!!")
                   else:
                       print("No fire :)")
-                      
-                  time.sleep(3.0)
+
+                  message = "temperature: " + str(temp) + " humidity: " + str(humid) + " CO2: " + str(COlevel)
+                  node.sendMessage(1, message)
+                  time.sleep(1.0)
 
 if __name__ =='__main__':
          try:
