585aa2afa4f8b6558678654e294267ab2493878f
Ben Howarth <benjamin.howarth@sjsu.edu>
2022-04-11 21:11:15 -0700

    Aws datatable added

--- AWS.py
+++ AWS.py
@@ -1,92 +1,101 @@
-from secrets import password,username
+from secrets import password, username
 import mysql.connector as mysql
 from tabulate import tabulate
 
 # Username and Password are hidden for security
 HOST = 'seniorprojectdatabase.cemghzzxxgym.us-west-1.rds.amazonaws.com'
-DATABASE = '' #'SeniorProjectDatabase'
 USER = username
 PASSWORD = password
 
-# connect to the database
-db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+def init():
+    global db_connection, cursor
+    # connect to the database
+    db_connection = mysql.connect(host=HOST, user=USER, password=PASSWORD)
 
-# get server information
-print(db_connection.get_server_info())
+    # get the db cursor
+    cursor = db_connection.cursor()
 
-# get the db cursor
-cursor = db_connection.cursor()
+    # get database information
+    cursor.execute("select database();")
+    database_name = cursor.fetchone()
+    print("[+] You are connected to the database:")
 
-# get database information
-cursor.execute("select database();")
-database_name = cursor.fetchone()
-print("[+] You are connected to the database:", database_name)
+    # create a new database called library
+    cursor.execute("create database if not exists datalog")
 
-# create a new database called library
-cursor.execute("create database if not exists datalog")
+    # use that database
+    cursor.execute("use datalog")
 
-# use that database 
-cursor.execute("use datalog")
-print("[+] Changed to `datalog` database")
+    # create a table
+    cursor.execute("""create table if not exists firetable (
+        `id` integer primary key auto_increment not null,
+        `time` timestamp not null,
+        `station` integer not null,
+        `temp` float not null,
+        `humid` integer not null,
+        `firestatus` boolean not null
+        )""")
 
-# create a table
-cursor.execute("""drop table firetable""")
-cursor.execute("""create table if not exists firetable (
-    `id` integer primary key auto_increment not null,
-    `time` timestamp not null,
-    `station` integer not null,
-    `temp` float not null,
-    `humid` integer not null,
-    `firestatus` boolean not null
-    )""")
-print("[+] Table `firetable` created")
+def insert(fires):
+    global db_connection, cursor
+    # iterate over books list
+    for fire in fires:
+        time = fire.get("time")
+        station = fire.get("station")
+        temp = fire.get("temp")
+        humid = fire.get("humid")
+        firestatus = fire.get("firestatus")
+        # insert each book as a row in MySQL
+        cursor.execute("""insert into firetable (time, station, temp, humid, firestatus) values (
+            %s, %s, %s, %s, %s
+        )
+        """, params=(time, station, temp, humid, firestatus))
 
-# insert some books
-fires = [
-    {
-        "time": "1999-01-28 12:01:02",
-        "station": 1,
-        "temp": 20.5,
-        "humid": 35,
-        "firestatus": False,
-    },
-    {
-        "time": "1998-01-29 12:03:04",
-        "station": 2,
-        "temp": 200.5,
-        "humid": 5,
-        "firestatus": True,
-    }
-]
+    # commit insertion
+    db_connection.commit()
 
-# iterate over books list
-for fire in fires:
-    time= fire.get("time")
-    station = fire.get("station")
-    temp = fire.get("temp")
-    humid = fire.get("humid")
-    firestatus = fire.get("firestatus")
-    # insert each book as a row in MySQL
-    cursor.execute("""insert into firetable (time, station, temp, humid, firestatus) values (
-        %s, %s, %s, %s, %s
-    )
-    """, params=(time, station, temp, humid, firestatus))
-    print(f"[+] Inserted the firetable: {station}")
+# main ioop
+def main():
+    global db_connection, cursor
+    init()
+    # insert some books
+    fires = [
+        {
+            "time": "1999-01-28 12:01:02",
+            "station": 1,
+            "temp": 20.5,
+            "humid": 35,
+            "firestatus": False,
+        },
+        {
+            "time": "1998-01-29 12:03:04",
+            "station": 2,
+            "temp": 200.5,
+            "humid": 5,
+            "firestatus": True,
+        }
+    ]
+    insert(fires)
 
-# commit insertion
-db_connection.commit()
+    # fetch the database
+    cursor.execute("select * from firetable")
+    
+    # get all selected rows
+    rows = cursor.fetchall()
 
-# fetch the database
-cursor.execute("select * from firetable")
+    # print all rows in a tabular format
+    print(tabulate(rows, headers=cursor.column_names))
 
-# get all selected rows
-rows = cursor.fetchall()
+    # close the cursor
+    cursor.close()
 
-# print all rows in a tabular format
-print(tabulate(rows, headers=cursor.column_names))
+    # close the DB connection
+    db_connection.close()
 
-# close the cursor
-cursor.close()
 
-# close the DB connection
-db_connection.close()
+if __name__ == '__main__':
+    try:
+        main()
+        pass
+    except KeyboardInterrupt:
+        pass
