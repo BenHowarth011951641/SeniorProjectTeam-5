82a90245d30c71f8cefac491718bbfcc5a1d2ff7
Your Name <you@example.com>
2022-05-02 20:10:21 -0700

    wake

--- basestationwake.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-while :
-do
-    sudo PYTHONPATH=/home/pi/pythonscripts python3 basestation.py >> /tmp/basestation.log 
-done
---
+++ runbasestation.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo PYTHONPATH=/home/pi/pythonscripts python3 basestation.py
